fddd6ecfc3e1cd934e2b3dc2c6649224a1fe6364
Gundi Manoj <gundi.manoj@AD.MSYSTECHNOLOGIES.COM>
2023-05-31 13:53:16 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="headerMainContainer">
+      <h3>Notes</h3>
+      <h6>
+        Welcome <span>Manoj Gundi</span>
+      </h6>
+    </div>
+  );
+}
+
+export default Header;
